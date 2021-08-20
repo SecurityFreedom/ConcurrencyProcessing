f1f3de66aa59bb5a89951bb0db659a36a57e3443
kangyunsik <re12io@ajou.ac.kr>
2021-08-20 18:34:35 +0900

    First Commit

---
+++ src/test/java/com/reserve/item/ItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserve.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a9d58ffeb23249059aa0d892b9c702ea1b863f86
kangyunsik <re12io@ajou.ac.kr>
2021-08-20 22:55:19 +0900

    Controller for login/register

--- src/main/java/com/reserve/item/controller/UserController.java
+++ src/main/java/com/reserve/item/controller/UserController.java
@@ -1,2 +1,50 @@
-package com.reserve.item.controller;public class UserController {
+package com.reserve.item.controller;
+
+import com.reserve.item.domain.User;
+import com.reserve.item.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/")
+    public String loginPage(Model model) {
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String loginProcess(UserLoginForm userLoginForm) {
+        Optional<User> user = userService.findUserById(userLoginForm.getId());
+        String password = userLoginForm.getPassword();
+        return user.map(m -> m.getPassword().equals(password) ? "main" : "redirect:/")
+                .orElseGet(() -> "redirect:/");
+    }
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerProcess(UserRegisterForm userRegisterForm) {
+        Optional<User> user = userService.findUserById(userRegisterForm.getId());
+        return user.map(m->"redirect:/register")
+                .orElseGet(()->{
+                    User newUser = new User();
+                    newUser.setId(userRegisterForm.getId());
+                    newUser.setName(userRegisterForm.getName());
+                    newUser.setPassword(userRegisterForm.getPassword());
+                    newUser.setEmail(userRegisterForm.getEmail());
+                    userService.join(newUser);
+                    return "redirect:/";
+                });
+    }
 }

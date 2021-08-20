207d3312c6b6c2f33962b6610765d5fbd1089ada
lsh0902 <hehesi2007@naver.com>
2021-08-20 20:16:23 +0900

    mysql connect

--- src/main/java/com/reserve/item/controller/MyController.java
+++ src/main/java/com/reserve/item/controller/MyController.java
@@ -1,10 +1,29 @@
 package com.reserve.item.controller;
 
+import com.reserve.item.domain.User;
+import com.reserve.item.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class MyController {
+    private final UserService userService;
+
+    @RequestMapping("/req")
+    @ResponseBody
+    public String reqAns(@RequestParam("name") String name) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail("heheisi@ia.com");
+        user.setId("idid");
+        user.setPassword("12345");
+        userService.join(user);
 
 
+        return "ok";
+    }
 }

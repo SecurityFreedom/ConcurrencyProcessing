06bb3a908e2ce2ac04a5230cbdec5073b27cc50c
lsh0902 <hehesi2007@naver.com>
2021-08-24 23:30:56 +0900

    front axios cors

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import "./header.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faTruckLoading, faJedi } from '@fortawesome/free-solid-svg-icons';
+import {faTruckLoading, faJedi, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 
 const Header = () => {
-
+    const clickHandler = (e) => {
+        e.target.className === 'li' ? console.log(e.target.childNodes[0].href) : console.log(e.target.href);
+    }
 
 
 
@@ -17,10 +19,17 @@ const Header = () => {
                 <h2 className="header__title">Concurrency</h2>
             </span>
 
-            <span className="menus">
+            <span className="menus" onClick={clickHandler}>
                 <li className="li"><Link className="menus__item" to="/">메인</Link></li>
                 <li className="li"><Link className="menus__item" to="/items">상품목록</Link></li>
                 <li className="li"><Link className="menus__item" to="/mypage">마이페이지</Link></li>
+                <li className="li">
+                    <Link className="menus__item" to="/mypage">
+                        <FontAwesomeIcon className="header__logo" icon={faShoppingCart}></FontAwesomeIcon>
+                        <span className="cart__item_num">0</span>
+                    </Link>
+                </li>
+
             </span>
         </div>
     );
--- front/src/components/itempage/ItemPage.jsx
+++ front/src/components/itempage/ItemPage.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const ItemPage = () => {
+    const [items, setItems] = useState([]);
+    useEffect(()=>{
+        async function getItems() {
+            const gettingItem = await axios.get('http://localhost:8080/items');
+            console.log(gettingItem);
+        }
+
+        getItems();
+    });
+
+    
+
     return (
         <div>
             <h1>Item!</h1>

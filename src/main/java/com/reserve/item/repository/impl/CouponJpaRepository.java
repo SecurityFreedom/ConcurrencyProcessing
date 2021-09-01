a7afcef81bb606f2bc8402c317a2bc1e27b94c9b
kangyunsik <re12io@ajou.ac.kr>
2021-09-01 18:53:53 +0900

    Add Repository Annotation

--- src/main/java/com/reserve/item/repository/impl/CouponJpaRepository.java
+++ src/main/java/com/reserve/item/repository/impl/CouponJpaRepository.java
@@ -2,9 +2,11 @@ package com.reserve.item.repository.impl;
 
 import com.reserve.item.domain.Coupon;
 import com.reserve.item.repository.CouponRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class CouponJpaRepository implements CouponRepository {
 
     @Override
--- src/main/java/com/reserve/item/repository/impl/OrderJpaRepository.java
+++ src/main/java/com/reserve/item/repository/impl/OrderJpaRepository.java
@@ -2,7 +2,9 @@ package com.reserve.item.repository.impl;
 
 import com.reserve.item.domain.Orders;
 import com.reserve.item.repository.OrderRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class OrderJpaRepository implements OrderRepository {
     @Override
     public void create(Orders orders) {

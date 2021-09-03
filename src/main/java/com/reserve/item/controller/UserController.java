89397fca2d0661285b66c54eaf5842e95d0086e7
kangyunsik <re12io@ajou.ac.kr>
2021-09-03 13:52:05 +0900

    API Test

--- src/main/java/com/reserve/item/controller/UserController.java
+++ src/main/java/com/reserve/item/controller/UserController.java
@@ -7,47 +7,54 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    @GetMapping(value = {"/", "/home"})
-    public String loginPage(Model model) {
-        return "home";
+    @PostMapping(value = {"/"})
+    @ResponseBody
+    public String processTest(){
+        return "ACCEPT!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     }
 
-    @PostMapping(value = {"/", "/home"})
-    public String loginProcess(UserLoginForm userLoginForm) {
-        String id = userLoginForm.getId();
-        String password = userLoginForm.getPassword();
-        Long userId = userService.login(id, password);
-
-        if (userId > 0) {
-            return "main";
-        } else {
-            return "redirect:/";
-        }
-    }
-
-    @GetMapping("/register")
-    public String registerPage(Model model) {
-        return "register";
-    }
-
-    @PostMapping("/register")
-    public String registerProcess(UserRegisterForm userRegisterForm) {
-        String id = userRegisterForm.getId();
-        String name = userRegisterForm.getName();
-        String password = userRegisterForm.getPassword();
-        String email = userRegisterForm.getEmail();
-
-        Long userId = userService.join(User.createUser(id,name,password,email));
-        if(userId>0){
-            return "redirect:/";
-        }else{
-            return "redirect:/register";
-        }
-    }
+//    @GetMapping(value = {"/", "/home"})
+//    public String loginPage(Model model) {
+//        return "home";
+//    }
+//
+//    @PostMapping(value = {"/", "/home"})
+//    public String loginProcess(UserLoginForm userLoginForm) {
+//        String id = userLoginForm.getId();
+//        String password = userLoginForm.getPassword();
+//        Long userId = userService.login(id, password);
+//
+//        if (userId > 0) {
+//            return "main";
+//        } else {
+//            return "redirect:/";
+//        }
+//    }
+//
+//    @GetMapping("/register")
+//    public String registerPage(Model model) {
+//        return "register";
+//    }
+//
+//    @PostMapping("/register")
+//    public String registerProcess(UserRegisterForm userRegisterForm) {
+//        String id = userRegisterForm.getId();
+//        String name = userRegisterForm.getName();
+//        String password = userRegisterForm.getPassword();
+//        String email = userRegisterForm.getEmail();
+//
+//        Long userId = userService.join(User.createUser(id,name,password,email));
+//        if(userId>0){
+//            return "redirect:/";
+//        }else{
+//            return "redirect:/register";
+//        }
+//    }
 }
